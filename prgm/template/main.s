a71da30b84099965d710feb5a1638e6cae4429a5
speedruneer <101725579+speedruneer@users.noreply.github.com>
2025-09-03 20:08:59 +0200

    better Template

--- prgm/template/main.s
+++ prgm/template/main.s
@@ -9,23 +9,9 @@
 jmp start
 
 ; data area
-process_ram_address: dd 0   ; placeholder variable
+
 
 
 ; Required area, do not touch
 
 start:
-    ; ----------------------------
-    ; Compute the runtime address of process_ram_address
-    ; ----------------------------
-    call get_eip             ; Push address of next instruction
-get_eip:
-    pop eax                  ; EAX = current instruction address
-    add eax, process_ram_address - get_eip  ; EAX = absolute RAM address of process_ram_address
-
-    ; Example: store a value
-    mov dword [eax], 0x12345678
-
-; Code
-
-; put whatever code for the program
