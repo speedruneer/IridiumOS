4c86a58c656ae96e0b049b1e16cb90ceaf3b0444
speedruneer <gaden.ilan@gmail.com>
2025-09-03 19:22:48 +0200

    I added error.inc, a few errors, not that many really

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -3,15 +3,15 @@ org 0x8900
 jmp kernel
 
 KRN_LOADED_STR: db 'krnld success', 0
-ERROR: db 'err: idk', 0
-
 
 %include "video.inc"
+%include "error.inc"
 
 kernel:
 
 clearScreen 0x0F
-printString 0, 0, KRN_LOADED_STR, 0x0F
+printString 0, 0, KRN_LOADED_STR, BLACK*BACKGROUND+RED*FOREGROUND
+
 
 hang:
     jmp hang
