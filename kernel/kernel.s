e7b262272790cdcf4a2e553e9aa4faaee3c11ecc
speedruneer <gaden.ilan@gmail.com>
2025-09-03 15:17:43 +0200

    Welp i got progress?

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -9,12 +9,9 @@ kernel:
 ;clearScreen 0x0F
 ;setChar 0, 0, 'E', 0x0F
 mov al, 13h
-call switchVgaOrTxt
-mov cl, 0x15
+call switchRenderingMode
+mov cl, 0x1
 call VGA_ClearScreen
-mov ax, 30
-mov bx, 30
-mov cl, 0Fh
-call VGA_PLOT_PIXEL
+
 hang:
-jmp hang
+    jmp hang
