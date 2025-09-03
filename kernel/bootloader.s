c1fe759e097b732654ee6f85c71f6dac61e02aa8
speedruneer <gaden.ilan@gmail.com>
2025-09-03 09:07:10 +0200

    Added plenty of files

---
+++ kernel/bootloader.s
@@ -0,0 +1,9 @@
+; -----------------------------------------------------------------------------
+; Copyright (c) 2025 SpeedruneerOff
+;
+; This source code is licensed under the MIT License.
+; See the LICENSE file in the project root for full license information.
+; -----------------------------------------------------------------------------
+
+times 510-($-$$) db 144
+dw 0xAA55
