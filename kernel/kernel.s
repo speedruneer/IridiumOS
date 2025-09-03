8e48f0100f1f3e862d2200698e49c2584c7abddc
speedruneer <gaden.ilan@gmail.com>
2025-09-03 15:07:28 +0200

    Welp VGA maybe?

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -2,11 +2,19 @@
 org 0x8900
 jmp kernel
 
-%include "video.inc"
+%include "vga.inc"
 
 kernel:
 
-clearScreen 0x0F
-setChar 0, 0, 'E', 0x0F
+;clearScreen 0x0F
+;setChar 0, 0, 'E', 0x0F
+mov al, 13h
+call switchVgaOrTxt
+mov cl, 0x15
+call VGA_ClearScreen
+mov ax, 30
+mov bx, 30
+mov cl, 0Fh
+call VGA_PLOT_PIXEL
 hang:
 jmp hang
